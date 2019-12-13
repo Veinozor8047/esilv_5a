d26a90e2d768272a54e8d66ddcd6c34767138267
romainjouin <romain.jouin@gmail.com>
2019-12-13 19:00:13 +0100

    Update django_prediction.py

--- django_prediction.py
+++ django_prediction.py
@@ -3,13 +3,14 @@ def predict_medv(unscaled_data):
     colonnes        = ["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM",
                         "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B",
                         "LSTAT"]
-    path_to_model   = "./ipynb/model_svr.pkl"
-    path_for_scaler = "./ipynb/scaler.pkl"
+    path_to_model   = ".pkl"
+    # on remet les data dans l'ordre des colonnes de la base d'apprentissage
     unscaled_data   = [unscaled_data[colonne] for colonne in colonnes]
+    # on load le modèle appris préalablement
     model           = joblib.load(path_to_model)
-    scaler          = joblib.load(path_for_scaler)
-    donnees_scalees = scaler.transform(unscaled_data)
+    # on prédit
     medv            = model.predict(donnees_scalees)
+    # on renvoie la prédiction
     return medv
 
 
